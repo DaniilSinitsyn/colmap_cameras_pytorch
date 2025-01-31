438a38eea406da04ca412bd6e065473a1dea832d
Ben Graham <btgraham@gmail.com>
2025-01-31 17:57:21 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="colmap_cameras_pytorch",
+    version="0.0.1",
+    description="colmap_cameras_pytorch",
+    url="https://github.com/DaniilSinitsyn/colmap_cameras_pytorch",
+    packages=["colmap_cameras"],
+)
